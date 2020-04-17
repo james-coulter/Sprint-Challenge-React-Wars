b2e84daed0fc0500924da2358ac66ceaacb99d9a
Christian Coulter <christiancoulter@Christians-MacBook-Air-2.local>
2020-04-17 09:45:38 -0600

    Finished fetching data from API and visualizing it through component. Need to set up props. Internet went down so trying to run everything on personal hotspot. Might take a bit longer than anticipated.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Character from './Character.js'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  //SELF NOTES: NO API_KEY TO INITIALIZE
+
+  // const api_request = 'https://swapi.py4e.com/api/people/'
+  
+  const [characters, setCharacters] = useState([]);
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+ const getAPI = async () => {
+   const response = await fetch ('https://swapi.py4e.com/api/people/')
+   const data = await response.json()
+   setCharacters(data.results)
+   console.log(data.results)
+
+ }
+
+  useEffect(() => {
+    // console.log('Effect is working!!')
+    getAPI()
+  }, [])
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
+    {characters.map(character => (
+      <Character />
+    ))}
     </div>
   );
 }
---
+++ starwars/src/Character.js
@@ -0,0 +1,16 @@
+// Write your Character component here
+import React from 'react'
+
+
+const Character = () => {
+    return(
+    <div>
+        <h1>name</h1>
+        <h3>height</h3>
+        <h3>hair_color</h3>
+    </div>
+    );
+
+}
+
+export default Character
